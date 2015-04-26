fc9c639c63db1fbc1068c040d4788d8464ab42b1
edonyM <edonyzpc@gmail.com>
2015-04-27 00:21:01 +0800

    lua basic func and comment

--- luafunc.lua
+++ luafunc.lua
@@ -6,6 +6,15 @@ function fact(n)
     end
 end
 
-print("enter the number: ")
+function twice(x)
+    return 2*x
+end
+
+function norm(x,y)
+    return (x^2 + y^2)^0.5
+end
+
+--[[print("enter the number: ")
 a = io.read("*number")
 print(fact(a))
+--]]
