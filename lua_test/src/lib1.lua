89a9b74a8ca06fb0077574c10c5a28567f09a01a
edonyM <edonyzpc@gmail.com>
2016-07-03 22:07:26 +0800

    lua exercise

---
+++ lua_test/src/lib1.lua
@@ -0,0 +1,3 @@
+function norm(x, y)
+  return (x^2 + y^2)^0.5;
+end
--- lua_test/src/main.lua
+++ lua_test/src/main.lua
@@ -1,3 +1,4 @@
+--[[first part]]--
 local function main()
   print("hello world\n")
   print("enter a number: \n")
@@ -6,3 +7,11 @@ local function main()
 
 end
 main()
+--[[ second part ]]--
+dofile("/Users/edony/coding/ilua/lua_test/src/lib1.lua") --[[ cannot load the current directory file]]--
+n = norm(3.14, 1.0)
+print(n)
+
+--[[third part]]--
+b = nil
+print(b)
